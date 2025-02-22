08484d37a4510bfdf0fbce1e770933e329a10a68
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2025-02-22 21:22:35 +0700

    Renamed the test class.

--- CurrencyTest.java
+++ CurrencyTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MyTest {
+public class CurrencyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
