df4cdee4131b5de9aef1699ca512ab02e1a237f5
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2025-03-02 05:35:48 +0700

    Added the content of Chapter 7 and 8.

--- CurrencyTest.java
+++ CurrencyTest.java
@@ -21,11 +21,20 @@ public class CurrencyTest {
 
     @Test
     public void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         // Triangulation
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
-        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
         // Triangulation
-        assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        // Franc vs. Dollar
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
+
+    @Test
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 }
